c3d98e5f66aba354bf6954abb62247389bbd10db
mortale2004 <pawanmortale111@gmail.com>
2023-03-01 18:59:34 +0530

    file name and footer

--- script/script.js
+++ script/script.js
@@ -101,3 +101,17 @@ function showPage() {
   document.getElementById("loader").style.display = "none";
   document.getElementById("myDiv").style.display = "block";
 }
+
+// To write the text in the choose file
+let file = document.getElementById("actual-btn");
+console.log(file);
+let print = document.getElementById("choose");
+file.addEventListener('change', function(){
+  let text = file.value;
+  text = text.replace("C:\\fakepath\\","");
+  print.innerHTML = text;
+})
+
+
+
+
